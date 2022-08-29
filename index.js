e67e56f13207d649c54920e7ee67973f09953c56
Julian Barker <julian.c.barker@gmail.com>
2022-08-29 16:15:27 -0700

    add background and text

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// declare shortcut function
+function $(selector) { return document.querySelector(selector) }
+
+function byId(id) { return document.getElementById(id) }
+
+function _(tag) { return document.createElement(tag) }
+
+
